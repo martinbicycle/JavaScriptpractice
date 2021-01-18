f7c04d3a525749eaba97b51a9e17ec7d7805333c
martinbicycle <martinbicycle7700@gmail.com>
2021-01-18 22:31:11 +0900

    [first commit]

---
+++ JS-12-Ajax/script.js
@@ -0,0 +1,31 @@
+API_KEY = "5b1f3d45f37bcfeb1b1f974b44ec857b"
+
+$(function(){
+  $('#btn').on('click', function() {
+
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function (data){
+
+      $('#place').text(data.name);
+
+      $('#temp_max').text(data.main.temp_max);
+
+      $('#temp_min').text(data.main.temp_min);
+
+      $('#humidity').text(data.main.humidity);
+
+      $('#speed').text(data.wind.speed);
+
+      $('#weather').text(data.weather[0].main);
+
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fail(function (data) {
+
+      alert('通信に失敗しました');
+    });
+  });
+});
+
